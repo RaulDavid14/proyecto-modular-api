2e9f684e38b2a5ed7ccf5e2edee2f23721be1adb
Raul David <raul.david@amentum.net>
2025-04-11 11:05:29 -0600

    cambio bd de prueba
    se cambia la bd de prueba del proyecto

--- cfca_api/configurations/credenciales.py
+++ cfca_api/configurations/credenciales.py
@@ -3,7 +3,7 @@ secret_key = 'django-insecure-8o(x50*__e38xc)f70!3q*2i)st=*w4$b=@59s0vz!aw%qi1-d
 
 
 db_prueba = {
-    'database' : 'CFCA'
+    'database' : 'cfca_dw'
     ,'user' : 'root'
     ,'password' : '1234'
     ,'host' : 'localhost'
